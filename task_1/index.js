d4ed55cbdf98d6cacf2cbc04c6bb1a3c63118674
waldymarxthf <iliya.nov223@gmail.com>
2023-10-06 12:36:41 +0300

    [done] done: test task

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,31 @@
 import {encoded, translations} from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+const decodeObject = (obj, translationMap, excludeKeys = []) => {
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => {
+      if (key.endsWith('Id') && !excludeKeys.includes(key)) {
+        return [key, translationMap[value] !== undefined ? translationMap[value] : value];
+      }
+      return [key, value];
+    })
+  );
+};
 
+const excludeKeys = ['groupId', 'service', 'formatSize', 'ca'];
 
+const decoded = encoded.map(obj => decodeObject(obj, translations, excludeKeys));
 
-// console.log(decoded)
+const uniqueIds = new Set();
+encoded.forEach(obj => {
+  Object.entries(obj).forEach(([key, value]) => {
+    if (key.endsWith('Id') && value !== null) {
+      uniqueIds.add(value);
+    }
+  });
+});
+
+// console.log("Let's rock");
+// console.log('Decoded:', decoded);
+
+console.log(decoded)
+console.log('Unique Ids:', Array.from(uniqueIds).sort());
